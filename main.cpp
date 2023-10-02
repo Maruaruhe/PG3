b411649dbe25e523d7ba6d44a23ec1bb9648ff0e
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-10-02 12:51:49 +0900

    da

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 ﻿#include <stdio.h>
 
-
-
+int SalaryPerHour(int n) {
+	if (n <= 1) {
+		return 100;
+	}
+	return ( (SalaryPerHour(n-1) * 2 - 50));
+}
+int TotalSalary(int n) {
+	if (n <= 1) {
+		return 100;
+	}
+	return (SalaryPerHour(n) + TotalSalary(n - 1));
+}
+//1 100
+//2 150  50         250
+//3 250  100       500
+//4 450  200       950
+//5 850  400       1800		 
+//6 1650  800
 int main() {
+	int n = 8;
+	int result;
+	int Normal = 1072;
+
+	result = TotalSalary(n);
+
+	printf("再帰的 : %d\n", result);
+	printf("一般的 : %d\n", Normal* n);;
 
 	return 0;
 }
