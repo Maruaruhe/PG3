8fe4a30229ea24c398f8ad99e07301ef5078e4c6
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-10-02 10:53:31 +0900

    dekita

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <stdio.h>
+
+
+template <typename Type>
+Type min(Type a, Type b) {
+	Type num;
+	if (a <= b) {
+		num = a;
+	}
+	else {
+		num = b;
+	}
+	return static_cast<Type>(num);
+}
+
+template<>
+char min<char>(char a, char b) {
+	return printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+}
+
 int main() {
+	printf("%d\n", min<int>(100, 200));
+	printf("%f\n", min<float>(10.0f, 20.0f));
+	printf("%lf\n", min<double>(30.0f, 20.0f));
+	printf("%c\n", min<char>( 81, 23));
 
 	return 0;
 }
