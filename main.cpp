89e98822026ffb6f97e291dbe9095c88b9cbc2c3
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-10-30 13:01:13 +0900

    s

--- main.cpp
+++ main.cpp
@@ -5,26 +5,26 @@
 
 void (*pfunc)();
 
-typedef void (*newType)(int *, int *);
+typedef void (*newType)(int , int );
 
 void SelectNum(newType n, int second) {
 	srand((unsigned int)time(NULL));
 	int rNum = rand() % 2 + 1;
-	printf("%d\n", rNum);
+	//printf("%d\n", rNum);
 	int num = 0;
 	printf("サイコロの出目が奇数と思うなら1,偶数と思うなら2を入力してください。\n");
 	scanf_s("%d", &num);
 
 	Sleep(second * 1000);
 
-	n(&rNum, &num);
+	n(rNum, num);
 }
 
-void checkAnswer(int* a, int* b) {
+void checkAnswer(int a, int b) {
 	if (a == b) {
-		printf("当たり\n");
+		printf("正解\n");
 	}else{
-		printf("はずれ\n");
+		printf("不正解\n");
 	}
 }
 
