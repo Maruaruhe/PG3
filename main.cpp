bdb0db12b66854a6e14cd4d82b7b98f3ed6902c5
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-10-31 14:02:21 +0900

    d

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <stdlib.h>
 #include <time.h>
+#include <functional>
 
 void (*pfunc)();
 
@@ -10,11 +11,14 @@ typedef void (*newType)(int , int );
 void SelectNum(newType n, int second) {
 	srand((unsigned int)time(NULL));
 	int rNum = rand() % 2 + 1;
+	
+	//std::function<int(int)> random=[int rNum]
+
 	//printf("%d\n", rNum);
 	int num = 0;
 	printf("サイコロの出目が奇数と思うなら1,偶数と思うなら2を入力してください。\n");
 	scanf_s("%d", &num);
-
+	
 	Sleep(second * 1000);
 
 	n(rNum, num);
@@ -28,11 +32,16 @@ void checkAnswer(int a, int b) {
 	}
 }
 
-int main() {
+int main(int argc,const char *argv[]) {
+	[]() {printf("test\n"); }();
+	//
+	std::function<int(int)> fx = [](int i) {return i + 1; };
+	printf("%d\n", fx(2));
 
-	newType n;
-	n = checkAnswer;
-	SelectNum(n, 3);
+
+	//newType n;
+	//n = checkAnswer;
+	//SelectNum(n, 3);
 
 
 	return 0;
