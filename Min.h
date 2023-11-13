2db3d74d2f5748a66405d31ee5575caf20f24a9c
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-11-13 11:22:01 +0900

    s

---
+++ Min.h
@@ -0,0 +1,24 @@
+#pragma once
+
+template <typename T1, typename T2>
+
+class Min
+{
+public:
+	Min(T1 num1, T2 num2) :num1_(num1), num2_(num2) {}
+
+
+	T2 GetMin() {
+		if (num1_ <= num2_) {
+			return static_cast<T1>(num1_);
+		}
+		else{
+			return static_cast<T2>(num2_);
+		}
+		return 0;
+	}
+private:
+	T1 num1_;
+	T2 num2_;
+};
+
--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <iostream>
+#include "Min.h"
+
 int main() {
+	Min<int,int> n1(22, 20);
+	Min<int, float> n2(10, 0.6f);
+	Min<int, double> n3(5, 3.6);
+	Min<float, float>n4(3.1f, 2.5f);
+	Min<float, double>n5(1.2f, 0.4);
+	Min<double, double>n6(3.6, 4.3);
+
+	std::cout << "int, int : "<< n1.GetMin() << std::endl;
+	std::cout << "int, float : "<< n2.GetMin() << std::endl;
+	std::cout << "int, double : "<< n3.GetMin() << std::endl;
+	std::cout << "float, float : "<< n4.GetMin() << std::endl;
+	std::cout << "float, double : "<< n5.GetMin() << std::endl;
+	std::cout << "double, double : "<< n6.GetMin() << std::endl;
 
 	return 0;
 }
