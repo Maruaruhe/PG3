8f53508af3d38e4514428c9b8117a3e5f5a16fd0
aruma <k022g0101@g.neec.ac.jp>
2024-02-12 15:07:11 +0900

    a

--- main.cpp
+++ main.cpp
@@ -10,19 +10,21 @@ void SelectNum(pFunc n, int second) {
 	int rNum = rand() % 2 + 1;
 	//printf("%d\n", rNum);
 	int num = 0;
-	printf("サイコロの出目が奇数と思うなら1,偶数と思うなら2を入力してください。\n");
+	printf("サイコロの出目が奇数と思うなら1,偶数と思うなら2を入力した後、エンターキーを押してください。\n");
 	scanf_s("%d", &num);
 
-	Sleep(second * 1000);
-
+	for (int i = 0; i < second; i++) {
+		printf("・");
+		Sleep(1000);
+	}
 	n(rNum, num);
 }
 
 void checkAnswer(int a, int b) {
 	if (a == b) {
-		printf("正解\n");
+		printf("\n正解\n");
 	}else{
-		printf("不正解\n");
+		printf("\n不正解\n");
 	}
 }
 
