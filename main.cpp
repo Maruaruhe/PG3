114df3db091f4e44d18c07fc5596f789910ace3e
aruma <k022g0101@g.neec.ac.jp>
2024-02-12 16:49:02 +0900

    s

--- main.cpp
+++ main.cpp
@@ -5,21 +5,6 @@
 
 typedef void (*pFunc)(int , int );
 
-void SelectNum(pFunc n, int second) {
-	srand((unsigned int)time(NULL));
-	int rNum = rand() % 2 + 1;
-	//printf("%d\n", rNum);
-	int num = 0;
-	printf("サイコロの出目が奇数と思うなら1,偶数と思うなら2を入力した後、エンターキーを押してください。\n");
-	scanf_s("%d", &num);
-
-	for (int i = 0; i < second; i++) {
-		printf("・");
-		Sleep(1000);
-	}
-	n(rNum, num);
-}
-
 void checkAnswer(int a, int b) {
 	if (a == b) {
 		printf("\n正解\n");
@@ -28,11 +13,26 @@ void checkAnswer(int a, int b) {
 	}
 }
 
+void SetTimeOut(pFunc n, int diceNum, int selectNum) {
+
+	for (int i = 0; i < 3; i++) {
+		printf("・");
+		Sleep(1000);
+	}
+	n(diceNum, selectNum);
+}
+
 int main() {
+	srand((unsigned int)time(NULL));
+	int diceNum = rand() % 2 + 1;
+
+	int num = 100;
+	printf("サイコロの出目が奇数と思うなら1,偶数と思うなら2を入力した後、エンターキーを押してください。\n");
+	scanf_s("%d", &num);
 
 	pFunc pCallBack;
 	pCallBack = checkAnswer;
-	SelectNum(pCallBack, 3);
+	SetTimeOut(pCallBack, diceNum, num);
 
 
 	return 0;
